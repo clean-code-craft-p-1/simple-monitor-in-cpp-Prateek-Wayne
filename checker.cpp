6473165fab8a514173e4c6882af7bf27560d515c
Prateek Wayne <prateek.12016050@lpu.in>
2023-08-30 10:12:51 +0530

    reduced Cyclomatic complexity

--- checker.cpp
+++ checker.cpp
@@ -3,44 +3,53 @@
 #include <unistd.h>
 using namespace std;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(temperature > 102 || temperature < 95) {
-    cout << "Temperature critical!\n";
+void printCriticalMessage(string message) {
+    cout << message << endl;
     for (int i = 0; i < 6; i++)
     {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
+        cout << "\r* " << flush;
+        sleep(1);
+        cout << "\r *" << flush;
+        sleep(1);
     }
+}
 
-    return 0;
-  } else if(pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
+bool checkTemperature(float temperature) {
+    if (temperature > 102 || temperature < 95) {
+        printCriticalMessage("Temperature critical!");
+        return false;
     }
-    return 0;
-  } else if(spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
-      cout << "\r* " << flush;
-      sleep(1);
-      cout << "\r *" << flush;
-      sleep(1);
+    return true;
+}
+
+bool checkPulseRate(float pulseRate) {
+    if (pulseRate < 60 || pulseRate > 100) {
+        printCriticalMessage("Pulse Rate is out of range!");
+        return false;
     }
-    return 0;
-  }
-  return 1;
+    return true;
+}
+
+bool checkSpo2(float spo2) {
+    if (spo2 < 90) {
+        printCriticalMessage("Oxygen Saturation out of range!");
+        return false;
+    }
+    return true;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+    return checkTemperature(temperature) && checkPulseRate(pulseRate) && checkSpo2(spo2);
 }
 
 int main() {
-  assert(!vitalsOk(99, 102, 70));
-  assert(vitalsOk(98.1, 70, 98));
-  cout << "Done\n";
+
+    assert(!vitalsOk(99, 102, 70));
+    assert(vitalsOk(98.1, 70, 98));//normal temperature,normal pulse,normal spo2
+    assert(!vitalsOk(103, 72, 95)); // high temperature
+    assert(!vitalsOk(94, 72, 95)); // low temperature
+    assert(!vitalsOk(98.6, 59, 95)); // low pulse rate
+    assert(!vitalsOk(98.6, 101, 95)); // high pulse rate
+    assert(!vitalsOk(98.6, 72, 89)); // low spo2
+    cout << "Done\n";
 }
